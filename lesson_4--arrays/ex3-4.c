8b76587767a037cad871c5410faba42bfa9083f9
fcsalles <felipecalheirossalles@gmail.com>
2026-09-04 22:53:32 -0300

    fixed and finished exercise 3, lesson 4

--- lesson_4--arrays/ex3-4.c
+++ lesson_4--arrays/ex3-4.c
@@ -9,3 +9,33 @@ int tradutor(int array1[], int array2[], int size) {
     }
     return array3[];
 }
+ 
+int main() {
+    int tamanho;
+    printf("quantidade de temperaturas: ");
+    scanf("%d", &tamanho);
+
+    int temperaturas1[tamanho];
+    int temperaturas2[tamanho];
+
+    printf("Digite %d temperaturas para o primeiro array:\n", tamanho);
+    for (int i = 0; i < tamanho; i++) {
+        scanf("%d", &temperaturas1[i]);
+    }
+
+    printf("Digite %d temperaturas para o segundo array:\n", tamanho);
+    for (int i = 0; i < tamanho; i++) {
+        scanf("%d", &temperaturas2[i]);
+    }
+
+    int resultado[tamanho];
+    tradutor(temperaturas1, temperaturas2, tamanho);
+
+    printf("As temperaturas traduzidas são:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", resultado[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
