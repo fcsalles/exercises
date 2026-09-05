3e92445cb3287a1c4cd8f6e516dadab0ed248ef6
fcsalles <felipecalheirossalles@gmail.com>
2026-09-04 22:51:28 -0300

    added exercise 3, lesson 4

---
+++ lesson_4--arrays/ex3-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int tradutor(int array1[], int array2[], int size) {
+    int array3[];
+    for (int i = 0; i < size; i++) {
+        if (array1[i] == array2[i]) {
+            array3[] += array1[i];
+        }
+    }
+    return array3[];
+}
